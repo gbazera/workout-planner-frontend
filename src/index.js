ec5f2b9797fa8684faaa4cb3bca5cad3c29cd22b
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-23 00:49:17 +0400

    Added auth context

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 import App from './App';
 
 import { RoutinesContextProvider } from './context/RoutinesContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RoutinesContextProvider>
-      <App />
-    </RoutinesContextProvider>
+    <AuthContextProvider>
+      <RoutinesContextProvider>
+        <App />
+      </RoutinesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
