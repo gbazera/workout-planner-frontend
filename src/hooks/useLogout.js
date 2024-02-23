b906d235f630569fed609daf691df77784a60935
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-23 13:39:51 +0400

    Clearing routines on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,13 +1,17 @@
 import { useAuthContext } from './useAuthContext'
+import { useRoutinesContext } from './useRoutinesContext'
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch } = useRoutinesContext()
 
     const logout = () => {
         localStorage.removeItem('user')
 
-        dispatch({
-            type: 'LOGOUT',
+        dispatch({ type: 'LOGOUT' })
+        workoutsDispatch({
+            type: 'SET_ROUTINES',
+            payload: null,
         })
     }
 
