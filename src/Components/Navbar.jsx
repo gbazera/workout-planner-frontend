404f04b7d6bc017b33c7ec2a0ce703dccfd8efc4
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-23 12:43:00 +0400

    Auth status fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
     const { logout } = useLogout()
+    const { user } = useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -18,24 +20,31 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="navbar-end">
-                <div className="navbar-item">
-                    <button
-                        className="button is-danger is-outlined"
-                        onClick={handleClick}
-                    >
-                        Log out
-                    </button>
-                </div>
-                <div className="navbar-item">
-                    <div className="buttons">
-                        <Link to={'/login'} className="button is-light">
-                            Login
-                        </Link>
-                        <Link to={'/signup'} className="button is-primary">
-                            Sign up
-                        </Link>
+                {user && (
+                    <>
+                        <Link className="navbar-item">{user.email}</Link>
+                        <div className="navbar-item">
+                            <button
+                                className="button is-danger is-outlined"
+                                onClick={handleClick}
+                            >
+                                Log out
+                            </button>
+                        </div>
+                    </>
+                )}
+                {!user && (
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <Link to={'/login'} className="button is-light">
+                                Login
+                            </Link>
+                            <Link to={'/signup'} className="button is-primary">
+                                Sign up
+                            </Link>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </nav>
     )
