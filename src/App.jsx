b4563bba1140d19f094e251062a043ecc9c63153
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-02-27 17:02:51 +0400

    Fixed titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { BrowserRouter, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Navbar from './Components/Navbar'
-
-import RouteWithTitle from './Components/RouteWithTitle'
 
 function App() {
     const { user } = useAuthContext()
@@ -18,9 +16,9 @@ function App() {
 
                 <div className="pages">
                     <Routes>
-                        <RouteWithTitle title='Workout Planner' path="/" element={user ? <Home /> : <Navigate to='/login' />} />
-                        <RouteWithTitle title='Login - Workout Planner' path="/login" element={!user ? <Login /> : <Navigate to='/' />} />
-                        <RouteWithTitle title='Signup - Workout Planner' path="/signup" element={!user ? <Signup /> : <Navigate to='/' />} />
+                        <Route path="/" element={user ? <Home /> : <Navigate to='/login' />} />
+                        <Route path="/login" element={!user ? <Login /> : <Navigate to='/' />} />
+                        <Route path="/signup" element={!user ? <Signup /> : <Navigate to='/' />} />
                     </Routes>
                 </div>
             </BrowserRouter>
